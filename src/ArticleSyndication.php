ebb81d9e7a31bfc70bbffa140f9dd54913969b9d
Timur Tripp <timur@timurtripp.com>
2024-12-03 10:43:14 -0700

    CuBoulder/ucb_article_syndication#4 Changes audience and unit form display to Autocomplete (Tags style)

--- src/ArticleSyndication.php
+++ src/ArticleSyndication.php
@@ -40,7 +40,7 @@ class ArticleSyndication {
     $hidden = $articleContentTypeConfig->get('hidden');
 
     $content['field_syndication_audience'] = [
-      'type' => 'entity_reference_autocomplete',
+      'type' => 'entity_reference_autocomplete_tags',
       'weight' => 98,
       'region' => 'content',
       'settings' => [
@@ -52,7 +52,7 @@ class ArticleSyndication {
       'third_party_settings' => [],
     ];
     $content['field_syndication_unit'] = [
-      'type' => 'entity_reference_autocomplete',
+      'type' => 'entity_reference_autocomplete_tags',
       'weight' => 99,
       'region' => 'content',
       'settings' => [
